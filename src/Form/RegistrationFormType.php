150845dd39e962f525be0c9633e39c2a6acd8d11
mandfredg <mandfred974@hotmail.fr>
2026-03-11 15:43:28 +0100

    Enhance email validation in registration form
    - Added validation constraints for the email field in the `RegistrationFormType` to ensure it is not blank and follows a valid email format, improving user input handling during registration.

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -17,7 +18,12 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', null, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Please enter your email.']),
+                    new Email(['message' => 'Please enter a valid email address.']),
+                ],
+            ])
             ->add('firstName', null, [
                 'label' => 'First name',
                 'constraints' => [new NotBlank(['message' => 'Please enter your first name.'])],
